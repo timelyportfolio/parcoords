3256601b38050e6714a7b11425932f40299016ea
timelyportfolio <kent.russell@timelyportfolio.com>
2017-12-30 11:48:57 -0600

    create prototype with categorical axis selection

--- inst/examples/experiments_outside_filters.R
+++ inst/examples/experiments_outside_filters.R
@@ -6,7 +6,8 @@ library(htmltools)
 # get a parcoords that we can hack later
 pc <- parcoords(
   mtcars,
-  brushMode = "1d"
+  brushMode = "1d",
+  height = 350
 )
 
 browsable(
@@ -45,4 +46,72 @@ d3.select('#select-cyl').on('change', function() {
 "
     ))
   )
+)
+
+
+# try categorical axis selection
+htmlwidgets::onRender(
+  pc,
+"
+function(el, x) {
+  var pc = this.instance.parcoords
+
+  // hard code for now cyl will be 3rd dimension
+  var dim_cyl = d3.select('.dimension:nth-of-type(3)')
+
+  var sc_cyl = pc.dimensions().cyl.yscale
+
+	var sc_y = d3.scale.ordinal()
+    .domain([4,6,8])
+    .rangeRoundBands(sc_cyl.range())
+
+  var ax_y = d3.svg.axis()
+    .scale(sc_y)
+    .orient('left')
+
+  dim_cyl.selectAll('.tick').remove()
+
+  var g_ax = dim_cyl.append('g')
+    .call(ax_y)
+
+  var color = d3.scale.category10()
+
+  g_ax.select('path').style('display', 'none')
+
+  g_ax
+  	.selectAll('.tick')
+    .classed('selected', true)
+  	.append('rect')
+  	.attr('height', sc_y.rangeBand())
+  	.attr('y', -sc_y.rangeBand()/2)
+  	.attr('width', 10)
+    .attr('x', -5)
+  	.attr('fill', function(d){return color(d)})
+
+  g_ax.selectAll('.tick')
+    .style('opacity', 1)
+  	.on('click', toggle)
+
+  function toggle() {
+    var selected = !d3.select(this).classed('selected')
+    d3.select(this).classed('selected', selected)
+    d3.select(this)
+    	.style('opacity', selected ? 1 : 0.4)
+
+    filter_pc(dim_cyl, 'cyl')
+  }
+
+  function filter_pc(el, dim) {
+    var selected = []
+    dim_cyl.selectAll('.selected').each(function(d) {
+      selected.push(d)
+    })
+    var filters = pc.outsideFilters() || {}
+
+    filters[dim] = selected
+    pc.outsideFilters(filters)
+    pc.brushUpdated(pc.selected())
+  }
+}
+"
 )
