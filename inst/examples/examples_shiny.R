ec6925fa35a537eadebb25ffbedb699b0fd83631
timelyportfolio <kent.russell@timelyportfolio.com>
2018-10-07 12:14:12 -0500

    add shiny proxy example with filter

--- inst/examples/examples_shiny.R
+++ inst/examples/examples_shiny.R
@@ -1,21 +1,45 @@
 library(parcoords)
 library(shiny)
 
-ui <- parcoordsOutput("pc")
+ui <- tagList(
+  textOutput("filteredstate", container=h3),
+  parcoordsOutput("pc")
+)
 
 server <- function(input, output, session) {
-  output$pc <- renderParcoords(
-    parcoords(mtcars)
-  )
+  rv <- reactiveValues(filtered = FALSE)
 
-  pcp <<- parcoordsProxy("pc")
-  pcFilter(
-    parcoordsProxy("pc"),
-    list(
-      cyl = c(6,8),
-      hp = list(gt = 200)
-    )
-  )
+  output$pc <- renderParcoords({
+    parcoords(mtcars)
+  })
+
+  observe({
+    # toggle between filtered and unfiltered every 2.5 seconds
+    invalidateLater(2500)
+    rv$filtered <- !isolate(rv$filtered)
+  })
+
+  observeEvent(rv$filtered, {
+    # create a proxy with which we will communicate between
+    #   Shiny and the parallel coordinates without a re-render
+    pcp <- parcoordsProxy("pc")
+
+    if(rv$filtered) {
+      pcFilter(
+        pcp,
+        list(
+          cyl = c(6,8),
+          hp = list(gt = 200)
+        )
+      )
+    } else {
+      pcFilter(pcp, list())
+    }
+  })
+
+  output$filteredstate <- renderText({
+    paste0("Filtered: ", rv$filtered)
+  })
 }
 
 shinyApp(ui = ui, server = server)
