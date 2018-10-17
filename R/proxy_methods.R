d02d992cc198d2d672decdc2089e494f6706043c
timelyportfolio <kent.russell@timelyportfolio.com>
2018-10-16 22:15:56 -0500

    add hide and unhide columns proxy methods

--- R/proxy_methods.R
+++ R/proxy_methods.R
@@ -39,3 +39,43 @@ pcCenter <- function(pc=NULL, dim = NULL) {
   invokeRemote(pc, "center", list(dim = dim))
   pc
 }
+
+#' Hide \code{parcoords} columns through \code{parcoordsProxy}
+#'
+#' @param pc \code{parcoordsProxy}
+#' @param dim \code{string} column(s) to hide.
+#'
+#' @return \code{parcoords_proxy}
+#' @export
+#'
+pcHide <- function(pc=NULL, dim = NULL) {
+  if(!inherits(pc, "parcoords_proxy")) {
+    stop(
+      paste0("expecting pc argument to be parcoordsProxy but got ", class(pc)),
+      call. = FALSE
+    )
+  }
+
+  invokeRemote(pc, "hide", list(dim))
+  pc
+}
+
+#' Unhide \code{parcoords} columns through \code{parcoordsProxy}
+#'
+#' @param pc \code{parcoordsProxy}
+#' @param dim \code{string} column(s) to hide.
+#'
+#' @return \code{parcoords_proxy}
+#' @export
+#'
+pcUnhide <- function(pc=NULL, dim = NULL) {
+  if(!inherits(pc, "parcoords_proxy")) {
+    stop(
+      paste0("expecting pc argument to be parcoordsProxy but got ", class(pc)),
+      call. = FALSE
+    )
+  }
+
+  invokeRemote(pc, "unhide", list(dim = dim))
+  pc
+}
--- javascript/src/methods.js
+++ javascript/src/methods.js
@@ -8,4 +8,40 @@ methods.center = function(dim) {
   this.center(dim);
 }
 
+methods.hide = function(dim) {
+  // append dim to currently hidden axes
+  if(!Array.isArray(dim)) {
+    dim = [dim];
+  }
+
+  // store brushExtents to reapply after render/updateAxes
+  var extents = null;
+  if(typeof(this.brushExtents) !== "undefined") {
+    extents = this.brushExtents();
+  }
+  this.hideAxis(dim).render().updateAxes()
+  if(extents) {
+    this.brushExtents(extents);
+  }
+}
+
+methods.unhide = function(dim) {
+  // remove dim from currently hidden axes
+  //  first convert dim to array if it is not one
+  if(!Array.isArray(dim)) {
+    dim = [dim];
+  }
+  var dims = this.hideAxis().filter(function(d) {dim.indexOf(d) !== -1});
+
+  // store brushExtents to reapply after render/updateAxes
+  var extents = null;
+  if(typeof(this.brushExtents) !== "undefined") {
+    extents = this.brushExtents();
+  }
+  this.hideAxis(dims).render().updateAxes()
+  if(extents) {
+    this.brushExtents(extents);
+  }
+}
+
 export default methods;
